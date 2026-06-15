goldelico/letux-kernel

usb3503: add debugging messages
Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

drivers/usb/misc/usb3503.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#define dev_info dev_err
+
 #include <linux/clk.h>
 #include <linux/i2c.h>
 #include <linux/gpio.h>
@@ -68,6 +70,7 @@ struct usb3503 {
 
 static int usb3503_reset(struct usb3503 *hub, int state)
 {
+	dev_info(hub->dev, "usb3503_reset %d\n", state);
 	if (!state && gpio_is_valid(hub->gpio_connect))
 		gpio_set_value_cansleep(hub->gpio_connect, 0);
 
@@ -86,6 +89,7 @@ static int usb3503_connect(struct usb3503 *hub)
 	struct device *dev = hub->dev;
 	int err;
 
+	dev_info(dev, "usb3503_connect\n");
 	usb3503_reset(hub, 1);
 
 	if (hub->regmap) {
@@ -177,6 +181,8 @@ static int usb3503_probe(struct usb3503 *hub)
 	u32 mode = USB3503_MODE_HUB;
 	const u32 *property;
 	int len;
+
+	dev_info(dev, "usb3503_probe\n");
 
 	if (pdata) {
 		hub->port_off_mask	= pdata->port_off_mask;
@@ -298,6 +304,7 @@ static int usb3503_probe(struct usb3503 *hub)
 				hub->gpio_reset, err);
 			return err;
 		}
+		dev_info(hub->dev, "reset gpio state %d\n", gpio_get_value(hub->gpio_reset));
 	}
 
 	usb3503_switch_mode(hub, hub->mode);

dragonbox-shop/retrode3-kernel

drivers/usb/misc/usb3503.c
@@ -15,6 +15,9 @@
 #include <linux/platform_data/usb3503.h>
 #include <linux/regmap.h>
 
+#undef dev_info
+#define dev_info dev_err
+
 #define USB3503_VIDL		0x00
 #define USB3503_VIDM		0x01
 #define USB3503_PIDL		0x02
@@ -57,6 +60,8 @@ static int usb3503_connect(struct usb3503 *hub)
 {
 	struct device *dev = hub->dev;
 	int err;
+
+	dev_info(dev, "usb3503_connect\n");
 
 	if (hub->regmap) {
 		/* SP_ILOCK: set connect_n, config_n for config */
@@ -171,6 +176,8 @@ static int usb3503_probe(struct usb3503 *hub)
 	enum gpiod_flags flags;
 	int len;
 
+	dev_info(dev, "usb3503_probe\n");
+
 	if (pdata) {
 		hub->port_off_mask	= pdata->port_off_mask;
 		hub->mode		= pdata->initial_mode;
@@ -275,6 +282,7 @@ static int usb3503_probe(struct usb3503 *hub)
 		/* Datasheet defines a hardware reset to be at least 100us */
 		usleep_range(100, 10000);
 		gpiod_set_consumer_name(hub->reset, "usb3503 reset");
+		dev_info(hub->dev, "reset gpio state %d\n", gpiod_get_value(hub->reset));
 	}
 
 	if (hub->port_off_mask && !hub->regmap)